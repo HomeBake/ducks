4c7b0ec3a7d706c695d60af70540b26c587167a1
oka <oka@kode.ru>
2025-03-31 17:14:13 +0200

    fix(ts): add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env,
+    },
+    resolve: {
+      alias: {
+        '@features': path.resolve(__dirname, './src/features'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+        '@entities': path.resolve(__dirname, './src/entities'),
+        '@widgets': path.resolve(__dirname, './src/widgets'),
+        '@app': path.resolve(__dirname, './src/app'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@processes': path.resolve(__dirname, './src/processes'),
+      },
+    },
+  };
+});
