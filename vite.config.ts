0e35e7614c62d0811e403bb4947f5c33a8041363
oka <oka@kode.ru>
2025-03-31 17:27:03 +0200

    fix(ts): ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
-    plugins: [react()],
     define: {
       'process.env': env,
     },
@@ -21,5 +21,9 @@ export default defineConfig(({ mode }) => {
         '@processes': path.resolve(__dirname, './src/processes'),
       },
     },
-  };
-});
+    plugins: [react()],
+    build: {
+      commonjsOptions: { transformMixedEsModules: true },
+    },
+  }
+})
